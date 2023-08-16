668aeb2f149528fc1cebc71fee176b31897d5bfc
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-16 11:52:01 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,49 +3,40 @@ class Post < ApplicationRecord
     validates :title, presence: true
   
     # Length validation: Validates the length of the content attribute's value.
-    validates :content, length: { minimum: 20, maximum: 5000 }
+    validates :content, length: { minimum: 10, maximum: 5000 }
   
     # # Numericality validation: Validates that the views attribute is a number.
-    # validates :views, numericality: true
+    validates :views, numericality: true
   
     # # Numericality validation with specific range:
-    # validates :likes, numericality: { greater_than: 0, less_than: 1000 }
+    validates :likes, numericality: { greater_than: 0, less_than: 1000 }
   
     # # Inclusion validation: Validates that the category attribute's value is in a specified list.
-    # validates :category, inclusion: { in: ['Technology', 'Travel', 'Food'] }
+    validates :category, inclusion: { in: ['Technology', 'Travel', 'Food'] }
   
     # # Exclusion validation: Validates that the status attribute's value is not in a specified list.
-    # validates :status, exclusion: { in: ['Draft', 'Archived'] }
+    validates :status, exclusion: { in: ['Draft', 'Archived'] }
   
     # # Format validation: Validates the slug attribute's value using a regular expression.
-    # validates :slug, format: { with: /\A[a-z0-9\-]+\z/, message: "Only lowercase letters, digits, and hyphens allowed" }
+    validates :slug, format: { with: /\A[a-z0-9\-]+\z/, message: "Only lowercase letters, digits, and hyphens allowed" }
   
     # # Uniqueness validation: Ensures the slug attribute's value is unique in the database.
-    # validates :slug, uniqueness: true
+    validates :slug, uniqueness: true
+
+    # # Validate booleans
+    validates :terms_of_service, acceptance: true
   
     # # Custom validation method: Define a custom method for validation.
-    # validate :custom_validation_method
+    validate :custom_validation
   
-    # # Conditional validation: Apply validation only if the post is published.
-    # validates :published_at, presence: true, if: :published?
   
-    # # Confirmation validation: Checks if two attributes match (usually for password confirmation).
-    # validates :password_confirmation, presence: true, if: :password_changed?
+    private
   
-    # private
+    def custom_validation
+      if views.present? && likes.present? && views < likes
+        errors.add(:base, "Views cannot be less than likes")
+      end
+    end
   
-    # def custom_validation_method
-    #   if condition
-    #     errors.add(:base, "Custom validation failed")
-    #   end
-    # end
-  
-    # def published?
-    #   status == 'Published'
-    # end
-  
-    # def password_changed?
-    #   # Condition for confirmation validation
-    # end
   end
   
